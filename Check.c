8e464894ce08c8b2aae4c584da65678578b3af35
Abhinav Raj <abhiraj61002@gmail.com>
2023-10-16 11:28:29 +0530

    edit

--- Check.c
+++ Check.c
@@ -5,7 +5,7 @@ void main()
     char c;
     printf("Enter a  character : ");
     scanf("%c",&c);
-    if((c == 'a')|| (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u'))
+    if((c == 'a')|| (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u') || ((c == 'A')|| (c == 'E') || (c == 'I') || (c == 'O') || (c == 'U'))) 
     {
         printf("%c is vowel",c);
     } 
