6ec63ed35ca4cb9192dad4badfdb74b72e11f2a6
Abhinav Raj <abhiraj61002@gmail.com>
2023-12-06 15:05:49 +0530

    new

---
+++ Arraydelete.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+    int n, count = 0;
+    printf("Enter number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n], temp[n];
+    if(n==0)
+    {
+        printf("No element inside the array.");
+    
+    }
+    else
+    {
+        printf("Enter elements in the array: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+ 
+    printf("Array Before Removing Duplicates:\n ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+ 
+
+    for (int i = 0; i < n; i++)
+    {
+        int j;
+        for (j = 0; j < count; j++)
+        {
+          if (arr[i] == temp[j])
+            break;
+        }
+        if (j == count)
+        {
+          temp[count] = arr[i];
+          count++;
+        }
+    }
+ 
+    printf("\nArray After Removing Duplicates:\n ");
+    for (int i = 0; i < count; i++)
+        printf("%d ", temp[i]);
+ 
+    return 0;
+}
+    }
+    
---
+++ Ptr.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+    int num = 20;
+    int *var = &num;
+    printf("%d",*var);
+}
