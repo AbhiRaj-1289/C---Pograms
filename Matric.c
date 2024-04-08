60d6011908c446a2c0fda879114c50c65c3fee84
Abhinav Raj <abhiraj61002@gmail.com>
2024-04-08 09:19:27 +0530

    new

--- Matric.c
+++ Matric.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-    int A[3][3], B[3][3], C[3][3], i, j;
+    int A[3][3], B[3][3], C[3][3], result[3][3],k, i, j;
     printf("Enter elements of first array : \n");
     for(i = 0; i < 3; i++)
     {
@@ -27,6 +27,15 @@ void main()
             C[i][j] = A[i][j] + B[i][j];
         }
     }
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            for(k=0; k<3; k++)
+            {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
     printf("After Addition Resultant Matric : \n");
     for(i = 0; i < 3; i++)
     {
@@ -36,4 +45,14 @@ void main()
         }
         printf("\n");
     }
+    printf("After Multiplication resultant matrix :");
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            printf("%d\t",result[i][j]);
+        }
+        printf("\n");
+    }
+}
 }
