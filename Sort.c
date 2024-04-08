b15da4e08e2fcb18b9e560106ed81be1c4e85243
Abhinav Raj <abhiraj61002@gmail.com>
2024-04-08 10:40:50 +0530

    new

--- Sort.c
+++ Sort.c
@@ -11,15 +11,23 @@ void main()
     }
     for(i = 0; i < 5; i++)
     {
-        for(j = i; j < 5; j++)
+        /*for(j = 0; j < 4-i; j++)
         {
-            if(strcmp(name[j], name[j+1])>0)
+            if(strcmp(name[j], name[j+1])>0)        //Ascending Order
             {
                 strcpy(temp, name[j]);
                 strcpy(name[j], name[j+1]);
                 strcpy(name[j+1], temp); 
             }
-        }
+        }*/
+        for(j = 0; j < 5; j++)
+        {
+            if(strcmp(name[j], name[j+1]) < 0)        //Descending Order
+            {
+                strcpy(temp, name[j]);
+                strcpy(name[j], name[j+1]);
+                strcpy(name[j+1], temp); 
+            }
     }
     for(i = 0; i < 5; i++)
     {
